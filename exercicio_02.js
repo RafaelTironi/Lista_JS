a7f5ab57c0c012d0835575349386b24699f37086
RafaelTironi-2233 <rafael_tironi1@estudante.sesisenai.org.br>
2026-03-17 09:53:04 -0300

    add: terminei o exercicio02

---
+++ exercicio_02.js
@@ -0,0 +1,20 @@
+/*
+A NASA precisa de sua ajuda para realizar um lançamento de foguete.
+Crie um algoritmo que faça uma contagem regressiva para o lançamento,
+iniciando em 10 e indo até 0.
+Ao final da contagem, exiba a mensagem:
+"Lançamento realizado!"
+*/
+
+function foguete_nasa() {
+
+    for (let i = 10; i > 0; i--) {
+        console.log(i);
+
+    }
+    console.log("Lançamento realizado");
+};
+
+
+foguete_nasa();
+c
