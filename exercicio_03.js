65e14016ccf304b5e2dbdccefc128f024c197905
RafaelTironi-Casa <rafael_tironi1@estudante.sesisenai.org.br>
2026-03-18 18:48:56 -0300

    add: comecei a 4, falta fazer o sort

--- exercicio_03.js
+++ exercicio_03.js
@@ -53,4 +53,4 @@ function balanco() {
     }
 }
 
-balanco();
+//balanco();
---
+++ exercicio_04.js
@@ -0,0 +1,16 @@
+/*
+
+Crie um algoritmo que solicite 4 valores inteiros ao usuário.
+Ao final, o programa deve mostrar os números em ordem decrescente.
+
+*/
+
+function ordem_valor() {
+let numero = []
+    for (i = 0; i < 5; i++) {
+
+    }
+
+};
+
+ordem_valor();
