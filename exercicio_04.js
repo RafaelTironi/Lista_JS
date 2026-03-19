3d09c797bc776dc59cdf46708e4f511753fd2922
RafaelTironi-2233 <rafael_tironi1@estudante.sesisenai.org.br>
2026-03-19 07:40:26 -0300

    add: terminei o exercicio04

--- exercicio_04.js
+++ exercicio_04.js
@@ -7,10 +7,19 @@ Ao final, o programa deve mostrar os números em ordem decrescente.
 
 function ordem_valor() {
     let numeros = []
+    let temp = 0
     for (i = 0; i < 4; i++) {
         let numero = parseInt(prompt("Digite um número:"));
         numeros.push(numero);
-    }
+
+        
+
+        numeros.sort();
+        numeros.reverse();
+    };
+    
+    console.log(numeros);
+
 
 };
 
